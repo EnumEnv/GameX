13e5cfa5995795e8d69e05102e24aac4ad84cffd
EnumEnv <plesko2mihael2@gmail.com>
2024-06-09 13:44:10 +0200

    client getting remote test 1

--- src/Core/XNet/init.lua
+++ src/Core/XNet/init.lua
@@ -1,3 +1,15 @@
+-- Services --
+local RunService = game:GetService("RunService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+-- Init --
+local RemoteFunction
+if RunService:IsServer() then
+	RemoteFunction = Instance.new("RemoteFunction")
+	RemoteFunction.Name = "XNET_REPLICATOR"
+	RemoteFunction.Parent = ReplicatedStorage
+end
+
 -- Module --
 local XNet = {}
 
@@ -6,8 +18,28 @@ local DataManager = require(script.DataManager)
 
 -- Module Functions --
 function XNet.GetRemoteEvent(name: string): DataManager.APIType
+	if RunService:IsClient() then
+		local recievedRemote
+		
+		ReplicatedStorage.XNET_Replicator.OnClientInvoke = function(remote)
+			recievedRemote = remote
+		end
+		
+		ReplicatedStorage.XNET_REPLICATOR:InvokeServer(name)
+
+		repeat
+			task.wait()
+		until recievedRemote
+
+		return recievedRemote
+	end
+
 	return DataManager.CreateRemoteEvent(name)
 end
+
+RemoteFunction.OnServerInvoke(function(player, name)
+	return XNet.GetRemoteEvent(name)
+end)
 
 function XNet.GetDataEnum(name: string, children: { string })
 	return DataManager.CreateDataEnum(name, children)
