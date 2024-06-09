d04441f8e2f0f9d5736f73312ea1aabbe68152ae
EnumEnv <plesko2mihael2@gmail.com>
2024-06-09 12:20:22 +0200

    Added boolean bit32 optimizations

---
+++ src/Core/XNet/Datatypes/Array.lua
@@ -0,0 +1,10 @@
+-- Module --
+local Array = {}
+
+-- Module Functions --
+function Array.AutoOptimize()
+    
+end
+
+-- End --
+return Array
---
+++ src/Core/XNet/Datatypes/Boolean.lua
@@ -0,0 +1,49 @@
+-- Module --
+local Boolean = {}
+
+-- Local Functions --
+local function PackTo32BitOne(boolean, lshift, disp)
+	return lshift and bit32.lshift(boolean and 1 or 0, disp) or (boolean and 1 or 0)
+end
+
+local function PackTo32BitMultiple(...)
+	local packedData = 0
+
+	for i, dataInstance in ipairs({ ... }) do
+		packedData = bit32.bor(packedData, bit32.lshift(dataInstance and 1 or 0, i - 1))
+	end
+
+	return packedData
+end
+
+local function UnpackFromBit32(bit32Data)
+	local unpackedData = {}
+
+	while bit32Data > 0 do
+		-- Check if the least significant bit is set
+		local booleanValue = bit32.band(bit32Data, 1) == 1
+
+		table.insert(unpackedData, booleanValue)
+        
+		-- Shift the bit32Data to the right to process the next bit
+		bit32Data = bit32.rshift(bit32Data, 1)
+	end
+
+	return unpackedData
+end
+
+-- Module Functions --
+function Boolean.AutoOptimizeOne(boolean, lshift, disp)
+	return PackTo32BitOne(boolean, lshift, disp)
+end
+
+function Boolean.AutoOptimizeMultiple(...)
+	return PackTo32BitMultiple(...)
+end
+
+function Boolean.ConvertFromBit32ToBool(bit32Data)
+	return UnpackFromBit32(bit32Data)
+end
+
+-- End --
+return Boolean
