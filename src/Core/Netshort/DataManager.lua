7d938753a2af14b98579d67398f1188deffc082c
EnumEnv <plesko2mihael2@gmail.com>
2024-06-09 11:23:30 +0200

    Can create

--- src/Core/Netshort/DataManager.lua
+++ src/Core/Netshort/DataManager.lua
@@ -1,13 +1,24 @@
 -- Module --
 local DataManager = {}
-local Remotes = {}
+local Remotes = {
+	RemoteEvents = {},
+	RemoteFunctions = {},
+}
 DataManager.__index = DataManager
 
 -- Types --
 export type SelfType = typeof(DataManager)
 export type APIType = SelfType
 
+-- Variables --
+local RemoteTypes = script.Parent.RemoteTypes
+
+local RemoteEvent = require(RemoteTypes.RemoteEvent)
+
 -- Module Functions --
+function DataManager.CreateRemoteEvent(name: string): RemoteEvent.SelfType
+	local newRemote = RemoteEvent.CreateRemoteEvent(name)
+end
 
 -- End --
 return DataManager
